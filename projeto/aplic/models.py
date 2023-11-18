876a043ca187b9067d45c641e62aeb33cedb1680
jnmorais <jsmorais2002@gmail.com>
2023-11-18 16:15:12 -0300

    html

--- projeto/aplic/models.py
+++ projeto/aplic/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Card(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
-    turma = models.CharField(max_length=50, null=True)  # Defina como nulo
+    turma = models.CharField(max_length=50, null=True) 
 
     def __str__(self):
         return self.title
