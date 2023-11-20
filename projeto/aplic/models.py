a115956954f7d0a697e976db7f37eec380904c37
jnmorais <jsmorais2002@gmail.com>
2023-11-19 23:43:04 -0300

    data e hora

--- projeto/aplic/models.py
+++ projeto/aplic/models.py
@@ -1,9 +1,18 @@
+import uuid
 from django.db import models
 
 class Card(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
-    turma = models.CharField(max_length=50, null=True) 
+    turma = models.CharField(max_length=50, null=True)
+    data = models.DateField(null=True)
+    hora = models.TimeField(null=True)
+    id_aleatorio = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
 
     def __str__(self):
         return self.title
+    
+for card in Card.objects.all():
+    card.id_aleatorio = uuid.uuid4()
+    card.save()
+
--- projeto/aplic/views.py
+++ projeto/aplic/views.py
@@ -8,20 +8,20 @@ class IndexView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         
-       
-        cards = Card.objects.all().order_by('turma')
+        # Obtém todos os cards e os ordena pelo campo 'turma'
+        cards = Card.objects.all().order_by('turma', 'data', 'hora')
         
-        
+        # Cria um dicionário para armazenar os cards por turma
         cards_por_turma = {}
 
-       
+        # Agrupa os cards por turma
         for card in cards:
             turma = card.turma
             if turma not in cards_por_turma:
                 cards_por_turma[turma] = []
             cards_por_turma[turma].append(card)
 
-        
+        # Adiciona os dados ao contexto
         context['turmas'] = cards_por_turma.keys()
         context['cards_por_turma'] = cards_por_turma
 
