f2ad7310349116797597850637c40d02c8deb467
JACKSON APARECIDO FARIA OLIVEIRA <jackson.oliveira2@granbery.edu.dti>
2023-11-27 19:58:14 -0300

    Classes

--- projeto/aplic/admin.py
+++ projeto/aplic/admin.py
@@ -1,4 +1,24 @@
 from django.contrib import admin
-from .models import Card
+from .models import User, Endereco, Postagem
 
-admin.site.register(Card)
+admin.site.register(Postagem)
+class PostInline(admin.TabularInline):
+    model = Postagem
+    readonly_fields = ('id',)
+    extra = 1
+    classes = ('collapse', )
+    list_display = ("assunto", "data", "turma", "responsavel",)
+
+admin.site.register(Endereco)
+class EnderecoInline(admin.TabularInline):
+    model = Endereco
+    readonly_fields = ('id',)
+    extra = 1
+    classes = ('collapse', )
+    list_display = ("cidade", "cep", "rua", "bairro", "numero_casa",)
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cpf', 'matricula', "cargo")
+    inlines = [EnderecoInline, PostInline]
--- projeto/aplic/models.py
+++ projeto/aplic/models.py
@@ -1,18 +1,52 @@
 import uuid
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+class TurmaChoices(models.IntegerChoices):
+    SEXTO = 1, '6° Ano'
+    SETIMO = 2, '7° Ano'
+    OITAVO = 3, '8° Ano'
+    NONO = 4, '9° Ano'
+    TODAS = 5, 'Todas'
 
-class Card(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField()
-    turma = models.CharField(max_length=50, null=True)
-    data = models.DateField(null=True)
-    hora = models.TimeField(null=True)
-    id_aleatorio = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+class SistemasChoices(models.IntegerChoices):
+    DIRETOR = 1, 'Diretor'
+    PROFESSOR = 2, 'Professor'
+
+class User(models.Model):
+    nome = models.CharField(_("Nome"), blank=False, max_length=50,)
+    cpf = models.CharField(_("cpf"), blank=False, max_length=11, unique=True)
+    matricula = models.CharField(_("Número Matricula"), blank=False, max_length=50,)
+    email = models.EmailField(_("Email"), blank=False, max_length=11, unique=True,)
+    cargo = models.IntegerField(choices=SistemasChoices.choices, default=SistemasChoices.PROFESSOR)
+
+    class Meta:    
+        verbose_name = _('Usuario')
+        verbose_name_plural = _('Usuarios')
+        ordering = ['id']
 
     def __str__(self):
-        return self.title
+        return self.nome 
+  
+class Endereco(models.Model):
+    cidade = models.CharField(_('Cidade'), max_length=200)
+    cep = models.CharField(_('CEP'),)
+    bairro = models.CharField(_('Bairro'), max_length=200)
+    rua = models.CharField(_('Rua'), max_length=200)
+    numero_casa = models.CharField(_('Número '),)
+    endereco_cliente = models.ForeignKey(User, blank=True, default=None, null=True, on_delete= models.DO_NOTHING)
+
+    class Meta:
+        verbose_name = _('Endereço')
+        verbose_name_plural = _('Endereços')
+
     
-for card in Card.objects.all():
-    card.id_aleatorio = uuid.uuid4()
-    card.save()
+    def __str__(self):
+        return f"Cidade: {self.cidade} | Bairro: {self.bairro} | Rua: {self.rua}"
+
+class Postagem(models.Model):
+    assunto = models.CharField(_("Assunto"), max_length=50)
+    data = models.DateTimeField(_("Data da postagem"), blank=False)
+    conteudo = models.TextField(_("Sobre a postagem"), max_length=500)
+    turma = models.IntegerField(choices=TurmaChoices.choices, default=TurmaChoices.TODAS)
+    responsavel = models.ForeignKey(User, blank=True, default=None, null=True, on_delete= models.DO_NOTHING)
 
